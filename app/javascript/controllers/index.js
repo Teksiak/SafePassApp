7c44ed001e9117d140b04950f8f688754c7b2f62
Teksiak <bednarowski.wojciech@gmail.com>
2025-07-13 15:00:44 +0200

    feat: Add copy, visibility toggle and clear search features.

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,3 +9,9 @@ application.register("toast", ToastController)
 
 import SearchController from "./search_controller"
 application.register("search", SearchController)
+
+import ClipboardController from "./clipboard_controller"
+application.register("clipboard", ClipboardController)
+
+import TogglePasswordController from "./toggle_password_controller"
+application.register("toggle-password", TogglePasswordController)
