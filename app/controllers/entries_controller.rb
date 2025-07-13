d0bb1644dfb2615cb05ce5f2fcc76cdbac48497f
Teksiak <bednarowski.wojciech@gmail.com>
2025-07-13 14:00:14 +0200

    feat: Add entries editing.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,36 +1,64 @@
 class EntriesController < ApplicationController
-    before_action :authenticate_user!
+  before_action :authenticate_user!
+  before_action :set_entry, only: %i[show edit update destroy]
 
-    def index
-        @entries = current_user.entries
-        @main_entry = @entries.first
-    end
+  def index
+    @entries = current_user.entries.order(:name)
+    @main_entry = @entries.first
+  end
 
-    def show
-        @entry = current_user.entries.find(params[:id])
-    end
+  def show
+  end
 
-    def new
-        @entry = Entry.new
-    end
+  def new
+    @entry = Entry.new
+  end
 
-    def create
-      @entry = current_user.entries.new(entry_params)
+  def create
+    @entry = current_user.entries.new(entry_params)
 
-      if @entry.save
-        flash.now[:notice] = "<strong>#{@entry.name}</strong> has been saved!".html_safe
-        respond_to do |format|
-          format.html { redirect_to root_path }
-          format.turbo_stream { }
-        end
-      else
-        render :new, status: :unprocessable_entity
+    if @entry.save
+      flash.now[:notice] = "<strong>#{@entry.name}</strong> has been saved!".html_safe
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.turbo_stream { }
       end
+    else
+      render :new, status: :unprocessable_entity
     end
+  end
 
-    private
+  def edit
+  end
 
-    def entry_params
-      params.expect(entry: [ :name, :url, :username, :password ])
+  def update
+    if @entry.update(entry_params)
+      flash.now[:notice] = "#{@entry.name} has been updated."
+      respond_to do |format|
+        format.html { redirect_to @entry }
+        format.turbo_stream { }
+      end
+    else
+      render :edit, stats: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @entry.destroy
+    flash.now[:notice] = "#{@entry.name} has been deleted."
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.turbo_stream { }
+    end
+  end
+
+  private
+
+  def entry_params
+    params.expect(entry: [ :name, :url, :username, :password ])
+  end
+
+  def set_entry
+    @entry = current_user.entries.find(params[:id])
+  end
 end
